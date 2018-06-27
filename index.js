eab2c918fe75f0c67f3530adfdaf8d9c1a16445d
GEGirlsWhoCode <kelly0costello@gmail.com>
2018-06-27 18:24:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.handler = function (event, context, callback) {
   var skill = alexa.handler(event, context);
 
 //Task 1: Add your app ID from Alexa
-  skill.appId = 'amzn1.ask.skill.9b1b227b-32be-47c3-8078-b3a48b8c3b57';
+  skill.appId = '<your app id>';
   skill.registerHandlers(handlers);
   skill.execute();
 };
@@ -17,40 +17,40 @@ exports.handler = function (event, context, callback) {
 var handlers = {
   'LaunchRequest': function () {
     console.log("inside of LaunchRequest");
-    var speech0utput = "Welcome to your skill"
-    this.response.speak(speech0utput).listen(speech0utput);
-    this.emit(':responseReady');
+    var speechOutput = "Welcome to your skill"
+    intent_context.response.speak(speechOutput).listen(speechOutput);
+    intent_context.emit(':responseReady');
   },
 
   //Entering our custom intent
   //Task 1: Replace <your-intent> with the intent you created through Alexa
-  'GetData': function () {
+  'GetSong': function () {
     var intent_context = this
-    console.log("inside GetData")
-    returnFact().then(function (body) {
-      var speech0utput = body.results[0].trackName
-      intent_context.response.speak(speech0utput).listen(speech0utput);
+    console.log("inside GetSong")
+    returnSong().then(function (body) {
+      var speechOutput = body.results[0].trackName
+      intent_context.response.speak(speechOutput).listen(speechOutput);
       intent_context.emit(':responseReady');
     },function (error) {
       console.log("You have an error in GetData")
-      var speech0utput = "Sorry your API returned an error"
-      intent_context.response.speak(speech0utput).listen(speech0utput);
+      var speechOutput = "Sorry your API returned an error"
+      intent_context.response.speak(speechOutput).listen(speechOutput);
       intent_context.emit(':responseReady');
     })
   },
 
   'Unhandled': function (){
     console.log("inside of unhandled");
-    var speech0utput = "There was an error"
-    this.response.speak(speech0utput).listen(speech0utput);
+    var speechOutput = "There was an error"
+    this.response.speak(speechOutput).listen(speechOutput);
     this.emit(':responseReady');
 
   }
 };
 
-var returnFact = function() {
+var returnSong = function() {
   var url = "https://itunes.apple.com/search?term=maroon+5&limit=1"
-  console.log("inside returnFact")
+  console.log("inside returnSong")
 
   return new Promise (function(resolve, reject){
     request.get(url, function (error, response, body){

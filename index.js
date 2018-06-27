12df271e400a496801bc6eb583847345c38c70e5
GEGirlsWhoCode <kelly0costello@gmail.com>
2018-06-27 18:11:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var request = require('request');
 exports.handler = function (event, context, callback) {
   var skill = alexa.handler(event, context);
 
-  skill.appId = 'amzn1.ask.skill.b0c22d7c-e50e-4aaf-9322-1c577dca2770';
+  skill.appId = '<your app id>';
   skill.registerHandlers(handlers);
   skill.execute();
 };
@@ -15,6 +15,7 @@ exports.handler = function (event, context, callback) {
 //List of Alexa handlers
 var handlers = {
   'LaunchRequest': function () {
+    var intent_context = this
     console.log("inside of LaunchRequest");
    var speechOutput = "Hello welcome to your skill!";
    this.response.speak(speechOutput).listen(speechOutput);
@@ -32,6 +33,7 @@ var handlers = {
 },
 
   'Unhandled': function (){
+    var intent_context = this
     console.log("inside of unhandled");
     var speechOutput = "I didn't understand that.  Please try again";
     this.response.speak(speechOutput).listen(speechOutput);
